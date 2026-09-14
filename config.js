481252869e87dd44fc1a5a346e8f06998b1065b3
pj9889 <beuti4196@gmail.com>
2026-09-14 02:23:49 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const affiliateLink = "https://paysale.g2afse.com/click?pid=125904&offer_id=7852&ref_id=lolo";
+const affiliateLink = "https://datinglocalsearch.com/tds/ae?tds_campaign=s7281sag&tdsId=s7281sag_r&s1=int&utm_source=int&utm_term=1&clickid={clickid}&subid={subid}&subid2={subid2}&affid=8b1c8c2e";
