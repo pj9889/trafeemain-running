b6504ce9133ed8b7cb7a18ce52f2cb1471033598
pj9889 <beuti4196@gmail.com>
2026-09-14 23:54:42 -0700

    Delete config.js

--- config.js
+++
@@ -1,1 +0,0 @@
-const affiliateLink = "https://datinglocalsearch.com/tds/ae?tds_campaign=s7281sag&tdsId=s7281sag_r&s1=int&utm_source=int&utm_term=1&clickid={clickid}&subid={subid}&subid2={subid2}&affid=8b1c8c2e";
